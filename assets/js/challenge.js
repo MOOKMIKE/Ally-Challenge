8435e45e12424453c8458e1c547c7c13413e482f
Michael Anderson <mook.xc@gmail.com>
2016-04-18 19:01:03 -0400

    Added Javascripti to populate rates from JSON

--- assets/js/challenge.js
+++ assets/js/challenge.js
@@ -1,3 +1,17 @@
-$(function () {
+$(document).ready(function() {
+  var populateRates = function (data) {
+    var $ratesTableBody = $('.ratesTable > tbody');
+
+    data.sort(function (a,b) {
+      return parseFloat(b.apy) - parseFloat(a.apy);
+    });
+
+    data.forEach(function (bank) {
+      $ratesTableBody.append('<tr' + (bank.name === 'URBank' ? ' class=urbank':'') + '><td class="bankName">' + bank.name + '</td><td class="apy">' + bank.apy + ' %</td><td class="earnings">$' + bank.earnings + '</td></tr>');
+    });
+
+  }
   
-})(jquery);
+  // Get json for rates table
+  var rates = $.get("/assets/js/code-test.json", populateRates);
+});
